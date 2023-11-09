ad71d9d9585b6bef206bfde315fc0d098e57abdf
jr-esparza <juanesparza.junior@gmail.com>
2023-11-08 23:52:36 -0600

    starter file

---
+++ final_project/starter.py
@@ -0,0 +1,15 @@
+# creates adjancy list given a file name
+# Ex. Input 
+# A -> [(B,5),(C,7)]
+# B -> [(C,7),(A,10)] // store it as a list of lists. 
+# Returns list of list containing the graph
+def create_graph(file):
+    return
+# run dijikstras algorithm passing the graph from create_graph and the starter, end location. 
+def dijikstras(graph, start, end):
+    return
+
+if __name__ == "__main__":
+    # here you will create the graph first, then prompt the user for start and end 
+    # and then give them the shortest path   
+    print("hello")
